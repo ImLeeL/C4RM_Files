a44801ce97f5228011c07690966686bcc2c2a34d
ImLeeL <1553379525@qq.com>
2026-07-13 08:27:56 +0800

    Update YFinance_File.py

--- Python_HW3_StarterFiles/YFinance_File.py
+++ Python_HW3_StarterFiles/YFinance_File.py
@@ -1,13 +1,9 @@
-import numpy as np
+import yfinance as yf
 import pandas as pd
 
-def YahooData2returns(YahooData=None,symbol='AAPL'):
-    # Input:
-    # YahooData = data from Yahoo Finance
-    # Output:
-    # returns = array of returns
-    # Steps:
-    # Extract 'Close' and symbol (This is a 2d column. Demo below.)
-    # Calculate and return the lagged returns
-    returns = np.array([0.01      , 0.00990099])
+def YahooData2returns(YahooData):
+    close_price = YahooData['Close']
+    pricevec = close_price.values
+    returns = pricevec[1:] / pricevec[:-1] - 1
+
     return returns
